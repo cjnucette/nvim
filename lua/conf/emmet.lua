ae566349da337809bad5acbc20c7f0cf60f0e6ac
cjnucette <cjnucette@gmail.com>
2022-02-24 16:36:19 -0400

    Revert to default mapping

--- lua/conf/emmet.lua
+++ lua/conf/emmet.lua
@@ -1,6 +1,7 @@
 vim.g.user_emmet_mode = 'a'
 vim.g.user_emmet_install_global = 0
-vim.g.user_emmet_leader_key = '<leader>e'
+-- vim.g.user_emmet_leader_key = ''
+-- vim.g.user_emmet_expandabbr_key = ','
 
 vim.cmd[[
   augroup Emmet
