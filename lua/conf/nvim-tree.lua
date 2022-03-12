94a18ef69600bc2ff841480a5ffc07a3319df42c
cjnucette <cjnucette@gmail.com>
2022-03-12 19:47:38 -0400

    Add nvim-tree plugin

--- lua/conf/nvim-tree.lua
+++ lua/conf/nvim-tree.lua
@@ -1,4 +1,6 @@
-local nvimtree = require('nvim-tree')
+local nvimtree_ok, nvimtree = pcall(require,'nvim-tree')
+if not nvimtree_ok then return end
+
 local map = require('utils').map
 local signs = require('utils').signs
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
@@ -9,13 +11,20 @@ local list = {
 }
 
 -- g options
-vim.g.nvim_tree_quit_on_open = 1 -- close tree after choosing a file
+-- vim.g.nvim_tree_quit_on_open = 1 -- close tree after choosing a file
 vim.g.nvim_tree_indent_markers = 1
 
 -- setup
 nvimtree.setup({
-  auto_close = true, -- quit nvim if tree is the last buffer
+  auto_close = true, -- quit nvim if nvimtree is the last buffer.
   update_cwd = true,
+  open_on_setup = true,
+  disable_netrw        = false,
+  hijack_netrw         = true,
+  hijack_directories   = {
+    enable = true,
+    auto_open = true,
+  },
   diagnostics = {
     enable = true,
     icons = {
@@ -31,6 +40,11 @@ nvimtree.setup({
       list = list,
     },
   },
+  actions = {
+    open_file = {
+      quit_on_open = true, -- close nvimtree when selecting a file.
+    }
+  }
 })
 
 -- mappings
