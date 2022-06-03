ba09d1c8bbc2c10ca894ddb3649e964ff2f868fc
Carlos Nucette <cjnucette@gmail.com>
2022-06-03 19:50:32 -0400

    Reset some option to the default, add completion to commandline

--- lua/conf/nvim-cmp.lua
+++ lua/conf/nvim-cmp.lua
@@ -17,18 +17,17 @@ cmp.setup({
       vim.fn['vsnip#anonymous'](args.body)
     end,
   },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<c-space>'] = cmp.mapping(cmp.mapping.complete({}), { 'i', 'c' }),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<c-space>'] = cmp.mapping.complete(),
     ['<C-y>'] = cmp.config.disable,
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({
-      select = true,
-    }),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true, }),
 
     -- tab completion
     ['<Tab>'] = cmp.mapping(function(fallback)
@@ -37,7 +36,8 @@ cmp.setup({
       elseif vim.fn['vsnip#available'](1) == 1 then
         feedkey('<Plug>(vsnip-expand-or-jump)', '')
       elseif has_words_before() then
-        cmp.complete()
+        -- cmp.complete()
+        cmp.select_next_item()
       else
         fallback()
       end
@@ -61,6 +61,7 @@ cmp.setup({
   -- order matter
   sources = {
     { name = 'nvim_lsp' },
+    { name = 'nvim_lsp_signature_help' },
     { name = 'nvim_lua' },
     { name = 'vsnip' },
     { name = 'path' },
@@ -70,4 +71,20 @@ cmp.setup({
     native_menu = false,
     ghost_text = true,
   },
+})
+
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  -- sources = { { name = 'cmdline' } }
+  sources = cmp.config.sources({
+    { name = 'cmdline' }
+  }, {
+    { name = 'buffer' }
+  })
 })
